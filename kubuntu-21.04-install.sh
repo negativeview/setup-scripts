c0b3cfb569cfc2c17ae575548051ebb0641b430f
Daniel Grace <negativeview@gmail.com>
2021-06-07 20:11:31 -0500

    Create kubuntu-21.04-install.sh
    First pass at post-installation instructions for 21.04

---
+++ kubuntu-21.04-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt update
+apt install apt-transport-https ca-certificates cuda-drivers curl gnupg lsb-release python3-pip
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt install docker-ce docker-ce-cli containerd.io
