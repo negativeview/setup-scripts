86aa827389df2cb48925c93c06e19c3672b74bde
Daniel Grace <negativeview@gmail.com>
2021-11-14 11:42:18 -0600

    Update kubuntu-20.04.3-install.sh
    Update how cuda is installed

--- kubuntu-20.04.3-install.sh
+++ kubuntu-20.04.3-install.sh
@@ -8,7 +8,13 @@ apt install apt-transport-https ca-certificates curl git
 apt install python3-pip
 
 # Cuda install
-apt install cuda-drivers
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.5.0/local_installers/cuda-repo-ubuntu2004-11-5-local_11.5.0-495.29.05-1_amd64.deb
+dpkg -i cuda-repo-ubuntu2004-11-5-local_11.5.0-495.29.05-1_amd64.deb
+apt-key add /var/cuda-repo-ubuntu2004-11-5-local/7fa2af80.pub
+apt update
+apt install cuda
 
 # Docker install.
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
